788bd83e4badca44d0ffa6cde3fd030755f2e280
shiva341 <shivatejaprof@gmail.com>
2021-06-04 18:10:45 +0530

    TODO LIST

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
+  const [task, setTask] = useState([]);
+  let randomtask = ['task1', 'task2', 'task3', 'task4'];
+
+  const addtask = () => {
+    let n = parseInt(Math.random() * randomtask.length);
+    setTask([...task, randomtask[n]]);
+  };
+
+  const del = index => {
+    task.splice(index, 1);
+    setTask([...task]);
+  };
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="container">
+      <section className="addtodo">
+        <h3>Add a Todo </h3>
+        <button onClick={addtask}>+</button>
+      </section>
+      {task.length > 0 ? (
+        task.map((item, index) => {
+          return (
+            <div key={index + 1} className="taskcon">
+              <p className="text">{item}</p>
+              <div className="butcon">
+                <button id="red" onClick={() => del(index)}>
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <div className="noevent">No Events</div>
+      )}
     </div>
   );
 }
